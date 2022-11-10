3c64f3176f95a8efcb8038195c0c5547ebdb067c
Marcius-Petrucelli <54067040+Marcius-Petrucelli@users.noreply.github.com>
2022-11-10 16:51:17 -0300

    Update 01-openssh.sh

--- scripts/01-openssh.sh
+++ scripts/01-openssh.sh
@@ -159,18 +159,18 @@ echo -n "Verificando as dependências do OpenSSH Server, aguarde... "
 #
 # Verificando se o script já foi executado mais de 1 (uma) vez nesse servidor
 # OBSERVAÇÃO IMPORTANTE: OS SCRIPTS FORAM PROJETADOS PARA SEREM EXECUTADOS APENAS 1 (UMA) VEZ
-#if [ -f $LOG ]
-#	then
-#		echo -e "Script $0 já foi executado 1 (uma) vez nesse servidor..."
-#		echo -e "É recomendado analisar o arquivo de $LOG para informações de falhas ou erros"
-#		echo -e "na instalação e configuração do serviço de rede utilizando esse script..."
-#		echo -e "Todos os scripts foram projetados para serem executados apenas 1 (uma) vez."
-#		sleep 5
-#		exit 1
-#	else
-#		echo -e "Primeira vez que você está executando esse script, tudo OK, agora só aguardar..."
-#		sleep 5
-#fi
+if [ -f $LOG ]
+	then
+		echo -e "Script $0 já foi executado 1 (uma) vez nesse servidor..."
+		echo -e "É recomendado analisar o arquivo de $LOG para informações de falhas ou erros"
+		echo -e "na instalação e configuração do serviço de rede utilizando esse script..."
+		echo -e "Todos os scripts foram projetados para serem executados apenas 1 (uma) vez."
+		sleep 5
+		exit 1
+	else
+		echo -e "Primeira vez que você está executando esse script, tudo OK, agora só aguardar..."
+		sleep 5
+fi
 #
 # Script de configuração do OpenSSH Server no GNU/Linux Ubuntu Server 22.04.x LTS
 # opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
